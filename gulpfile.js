976a187f1c7cc873751ac8c15253bbf2efb9e925
Kamil Stawski <stawski.kamil@gmail.com>
2017-02-17 20:19:50 +0100

    Structure update, gulpfile update, index.html update

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var gulp = require('gulp');
 
 gulp.task('default', ['watch']);
 
-gulp.task('build-css', function() {
-    return gulp.src('public/css/**/*.scss')
+gulp.task('build-core', function() {
+    return gulp.src('public/assets/css/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('public/css/'));
+        .pipe(gulp.dest('public/assets/css/'));
 });
 
 /* watch task sass */
 gulp.task('watch', function() {
-    gulp.watch('public/css/**/*.scss', ['build-css']);
+    gulp.watch('public/assets/css/**/*.scss', ['build-core']);
 });
