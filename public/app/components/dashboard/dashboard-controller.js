95d90b05729a76f5d0c6f90fc4275e4c9fcade1f
adrian.wolanski <adrian.wolanski@gmail.com>
2017-03-15 14:21:04 +0100

    [F] - sorting expenses per date

--- public/app/components/dashboard/dashboard-controller.js
+++ public/app/components/dashboard/dashboard-controller.js
@@ -1,3 +1,5 @@
+var MOUNTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 angular.module('hhsApp').controller('DashboardController', DashboardController);
 
 DashboardController.$inject = ['$window', '$location', '$q', 'AuthFactory', 'DashboardService'];
@@ -6,6 +8,30 @@ function DashboardController($window, $location, $q, AuthFactory, DashboardServi
     var vm = this;
     vm.user = JSON.parse($window.sessionStorage.getItem('userData'));
     vm.expenses = [];
+    vm.mountsList = MOUNTHS;
+    vm.yearsList = [];
+
+    function setCurrentDataFilters() {
+        var todayDate = new Date();
+        var currentMonth = todayDate.getMonth();
+        var currentYear = todayDate.getFullYear();
+
+        vm.selectedMonth = MOUNTHS[currentMonth];
+        vm.selectedYear = currentYear;
+
+        filterExpenses();
+    }
+
+    function getYearsFromExpenses(expenses) {
+        var tYearList = [];
+
+        _.forEach(expenses, function (expense) {
+            var year = new Date(expense.expenseDate).getFullYear();
+            tYearList.push(year);
+        });
+
+        return _.uniq(tYearList);
+    }
 
     function setCategoryForExpense(expenses, categories) {
         angular.forEach(expenses, function (item) {
@@ -19,6 +45,38 @@ function DashboardController($window, $location, $q, AuthFactory, DashboardServi
         })
     }
 
+    function filterExpenses() {
+
+        console.log(vm.selectedMonth, vm.selectedYear);
+
+        function checkYear(e) {
+            var eYear = new Date(e.expenseDate).getFullYear();
+            if(eYear === vm.selectedYear) {
+                return true;
+            }
+
+            return false;
+        }
+
+        function checkMonth(e) {
+            var eMonthIndex = new Date(e.expenseDate).getMonth();
+            var eMonth = MOUNTHS[eMonthIndex];
+            if(eMonth === vm.selectedMonth) {
+                return true;
+            }
+
+            return false;
+        }
+
+        vm.filteredExpenses = [];
+
+        _.forEach(vm.expenses, function (e) {
+            if(checkYear(e) && checkMonth(e)) {
+                vm.filteredExpenses.push(e);
+            }
+        });
+    }
+
     function init() {
         $q.all([
             DashboardService.getAll(vm.user.id),
@@ -28,6 +86,8 @@ function DashboardController($window, $location, $q, AuthFactory, DashboardServi
             vm.categories = res[1].data;
 
             setCategoryForExpense(vm.expenses, vm.categories);
+            vm.yearsList = getYearsFromExpenses(vm.expenses);
+            setCurrentDataFilters();
         });
     }
 
@@ -47,5 +107,9 @@ function DashboardController($window, $location, $q, AuthFactory, DashboardServi
 
     vm.userProfile = function () {
         $location.path('/user/profile');
+    };
+
+    vm.filterExp = function () {
+        filterExpenses()
     };
 };
