75f3cf68c052e474343f797869f6e167d4be2145
adrian.wolanski <adrian.wolanski@gmail.com>
2017-03-31 13:41:01 +0200

    [B] Allow headers

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ var routes = require('./api/routes');
 app.set('port', (process.env.PORT || 1337));
 
 app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers","Content-Type, Authorization");
     console.log(req.method, req.url);
     next();
 });
+
+
 
 app.use(bodyParser.urlencoded({
     extended: false
